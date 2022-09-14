8a2bc6ac006a1843429c959e8a7f1e807076f187
Shaban <shabanxhemajli@live.com>
2022-09-14 13:49:22 +0200

    first commit

---
+++ countdown.py
@@ -0,0 +1,24 @@
+import time
+
+input_var=int(input("put in anynumber:"))
+cool_phrase="wait X#X#"
+ending_phrase="you made it, congats :))))"
+if input_var=="10":
+    time.sleep(0.8)
+print("9",cool_phrase)
+time.sleep(2) 
+print("8",cool_phrase)   
+time.sleep(3) 
+print("7",cool_phrase)
+time.sleep(1) 
+print("6",cool_phrase)
+time.sleep(2) 
+print("5",cool_phrase)
+time.sleep(2) 
+print("4",cool_phrase)
+time.sleep(1) 
+print("3",cool_phrase)
+time.sleep(2) 
+print("2",cool_phrase)
+time.sleep(3) 
+print("#1#",ending_phrase)
